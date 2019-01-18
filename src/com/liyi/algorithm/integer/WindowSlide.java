f10bd35b8ae7ed681b01ae09ea8a55741cd0d1c7
unknown <294206591@qq.com>
2019-01-18 15:13:25 +0800

    窗口滑动

---
+++ src/com/liyi/algorithm/integer/WindowSlide.java
@@ -0,0 +1,31 @@
+package com.liyi.algorithm.integer;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class WindowSlide {
+    public static void main(String[] args) {
+        getMax(new int[]{4,3,5,4,3,3,6,7},3);
+    }
+
+    public static void getMax(int[] arr,int size){
+        if(arr == null || arr.length == 0 || arr.length < size || size < 1) return ;
+        LinkedList<Integer> list = new LinkedList<>();
+        int[] res = new  int[arr.length - size + 1];
+        int index = 0;
+        for(int i = 0;i<arr.length;i++){
+            while(!list.isEmpty() && arr[list.peekLast()] <= arr[i]){
+                list.pollLast();
+            }
+            list.add(i);
+            if(list.peekFirst() == i-size ){
+                list.pollFirst();
+            }
+            if(i >= size - 1){
+                res[index++] = arr[list.peekFirst()];
+            }
+        }
+
+        System.out.println(Arrays.toString(res));
+    }
+}
