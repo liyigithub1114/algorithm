4e5aa4185d35477608cb05646a1458038ec394a1
liyi <294206591@qq.com>
2019-02-19 22:23:02 +0800

    提交数组内最小包含字串的窗口

--- src/com/liyi/algorithm/strings/MinimumWindowSubString.java
+++ src/com/liyi/algorithm/strings/MinimumWindowSubString.java
@@ -1,13 +1,21 @@
 package com.liyi.algorithm.strings;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Input: S = "ADOBECODEBANC", T = "ABC"
  * Output: "BANC"
+ *
+ * map 存储 T中所有字符串出现的次数
+ * mapS 当且仅当map中存在
  */
 public class MinimumWindowSubString {
     public static void main(String[] args) {
-
+        String s= "aa";
+        String t = "aa";
+        System.out.println(getWindow(s+1,t));
     }
     public static String  getWindow(String s,String t){
         if(s == null || t == null || s.length() < t.length()) return "";
@@ -15,9 +23,46 @@ public class MinimumWindowSubString {
         int lenT = t.length();
         int left = 0;
         int right = 0;
-        while(left < lenS && right < lenS){
-
+        String res = s + "1";
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        Map<Character,Integer> map = new HashMap<>();
+        Map<Character,Integer> mapS = new HashMap<>();
+        for(int i =0;i<lenT;i++){
+            if(map.containsKey(charT[i])){
+                map.put(charT[i],map.get(charT[i])+1);
+            }else{
+                map.put(charT[i],1);
+            }
         }
-        return "";
+        int count =0;
+        while(left < lenS && right < lenS){
+            //到第一个合法字串的时候right的值
+            while(count < lenT && right < lenS){
+                char temp = charS[right];
+                if(mapS.containsKey(temp)){
+                    mapS.put(temp,mapS.get(temp)+1);
+                }else{
+                    mapS.put(temp,1);
+                }
+                if(map.containsKey(temp) && mapS.get(temp) <= map.get(temp)){
+                    count++;
+                }
+                right++;
+            }
+            //当第一次不合法时left的值
+            while(count == lenT && left < lenS){
+                char temp = charS[left];
+                if(map.containsKey(temp)){
+                    mapS.put(temp,mapS.get(temp) - 1);
+                    if(mapS.get(temp) < map.get(temp)){
+                        count--;
+                    }
+                }
+                left++;
+            }
+            res = res.length() > right - left && left-1 >= 0 ? s.substring(left-1,right) : res;
+        }
+        return  res.length() == s.length()+1 ? "":res;
     }
 }
