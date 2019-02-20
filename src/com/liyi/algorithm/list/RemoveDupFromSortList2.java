f7b04f8aab274d5d44f18ecc37f293456bd4330b
unknown <294206591@qq.com>
2019-02-20 20:26:37 +0800

    链表去重 + 单调栈

--- src/com/liyi/algorithm/list/RemoveDupFromSortList2.java
+++ src/com/liyi/algorithm/list/RemoveDupFromSortList2.java
@@ -1,4 +1,52 @@
 package com.liyi.algorithm.list;
 
+/**
+ * intput:1->1->2->3->3->4
+ * output:2->4
+ */
 public class RemoveDupFromSortList2 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(3);
+        head.next.next.next.next.next = new ListNode(3);
+        ListNode node = deleteListNode(head);
+        while(node != null){
+            System.out.print(node.value + "->");
+            node = node.next;
+        }
+    }
+
+    public static ListNode deleteListNode(ListNode head) {
+        if (head == null) return head;
+        ListNode node = head;
+        ListNode res = null;
+        ListNode resTail = null;
+        boolean flag = false;//第一个假设不是重复的
+        while (node != null) {
+            ListNode next = node.next;
+            node.next = null;
+            if (next != null && node.value == next.value) {
+                flag = true;
+            } else {
+                if (flag) {
+                    //node有重复
+                    flag = false;
+                } else {
+                    //node无重复
+                    if (res == null) {
+                        res = node;
+                        resTail = node;
+                    } else {
+                        resTail.next = node;
+                        resTail = node;
+                    }
+                }
+            }
+            node = next;
+        }
+        return res;
+    }
 }
--- src/com/liyi/algorithm/strings/MaxRectangle.java
+++ src/com/liyi/algorithm/strings/MaxRectangle.java
@@ -1,4 +1,51 @@
 package com.liyi.algorithm.strings;
 
+import java.util.Stack;
+
+/**
+ * Input:
+ * [
+ *   ["1","0","1","0","0"],
+ *   ["1","0","1","1","1"],
+ *   ["1","1","1","1","1"], --> 3,1,3,2,2
+ *   ["1","0","0","1","0"]
+ * ]
+ * Output: 6
+ */
 public class MaxRectangle {
+    public static void main(String[] args) {
+        char temp = '0';
+        System.out.println(temp+0);
+    }
+
+    /**
+     *单调栈问题
+     *
+     */
+    public static int getMax(char[][] matrix){
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int res = 0;
+        int lenX = matrix.length;
+        int lenY = matrix[0].length;
+        int[] rectangle = new int[lenY];
+        for(int i =0;i<lenX;i++){
+            for(int j =0;j< lenY;j++){
+                if(matrix[i][j] == '0'){
+                    rectangle[j] = 0;
+                }else{
+                    rectangle[j] += matrix[i][j];
+                }
+            }
+            res = Math.max(res,getMax(matrix));
+        }
+        return res;
+    }
+
+    public static int getMax(int[] matrix){
+        if(matrix == null || matrix.length == 0) return 0;
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+
+        return res;
+    }
 }
